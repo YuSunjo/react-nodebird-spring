1ea3c4c896c94adfdceae5d0a38619af4f43bf69
yusunjo <tnswh2023@gmail.com>
2021-03-01 21:38:04 +0900

    feat(member): member domain, dev env

---
+++ potato-domain/src/main/java/com/potato/domain/member/repository/Member.java
@@ -0,0 +1,27 @@
+package com.potato.domain.member.repository;
+
+import com.potato.domain.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String name;
+
+    private String profileUrl;
+
+    @Enumerated(EnumType.STRING)
+    private MemberProvider provider;
+
+}
---
+++ potato-domain/src/main/java/com/potato/domain/member/repository/MemberProvider.java
@@ -0,0 +1,7 @@
+package com.potato.domain.member.repository;
+
+public enum MemberProvider {
+
+    GOOGLE
+
+}
