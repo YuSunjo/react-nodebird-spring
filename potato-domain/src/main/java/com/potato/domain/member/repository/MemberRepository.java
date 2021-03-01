21c597ee8220fb10705b65d90ed9ff1094089ceb
yusunjo <tnswh2023@gmail.com>
2021-03-01 21:38:04 +0900

    feat(member): member domain, dev env

---
+++ potato-domain/src/main/java/com/potato/domain/member/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.potato.domain.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
