8fae4b04b500a8d846e899808a11cccbc8141aba
yusunjo <tnswh2023@gmail.com>
2021-03-11 19:24:03 +0900

    feat(comment): 댓글 불러오기

--- potato-api/src/test/java/com/potato/service/comment/BoardCommentServiceTest.java
+++ potato-api/src/test/java/com/potato/service/comment/BoardCommentServiceTest.java
@@ -4,16 +4,20 @@ import com.potato.domain.board.Board;
 import com.potato.domain.board.BoardCreator;
 import com.potato.domain.board.repository.BoardRepository;
 import com.potato.domain.comment.BoardComment;
+import com.potato.domain.comment.BoardCommentCreator;
 import com.potato.domain.comment.BoardCommentRepository;
 import com.potato.domain.member.MemberCreator;
 import com.potato.service.MemberSetupTest;
 import com.potato.service.comment.dto.request.AddCommentRequest;
+import com.potato.service.comment.dto.response.BoardCommentResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,8 +34,18 @@ public class BoardCommentServiceTest extends MemberSetupTest {
     @Autowired
     BoardRepository boardRepository;
 
+    Board board;
+
+    @BeforeEach
+    void setupBoard() {
+        String boardContent = "boardContent입니다.";
+        board = BoardCreator.create(boardContent, memberId);
+        boardRepository.save(board);
+    }
+
     @AfterEach
     void cleanUp() {
+        super.cleanup();
         boardCommentRepository.deleteAll();
         boardRepository.deleteAll();
     }
@@ -52,6 +66,24 @@ public class BoardCommentServiceTest extends MemberSetupTest {
         //then
         List<BoardComment> boardCommentList = boardCommentRepository.findAll();
         assertThat(boardCommentList.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    void 댓글들을_가져옵니다() {
+        //given
+        String content1 = "content1";
+        String content2 = "content2";
+
+        BoardComment boardComment1 = BoardCommentCreator.create(content1, memberId, board.getId());
+        BoardComment boardComment2 = BoardCommentCreator.create(content2, memberId, board.getId());
+        boardCommentRepository.saveAll(Arrays.asList(boardComment1, boardComment2));
+
+        //when
+        List<BoardCommentResponse> responses = boardCommentService.getComment(board.getId(), memberId);
+
+        //then
+        assertThat(responses.get(0).getContent()).isEqualTo(content1);
+        assertThat(responses.get(1).getContent()).isEqualTo(content2);
     }
 
 }
--- potato-domain/src/main/java/com/potato/domain/comment/repository/BoardCommentRepositoryCustomImpl.java
+++ potato-domain/src/main/java/com/potato/domain/comment/repository/BoardCommentRepositoryCustomImpl.java
@@ -1,11 +1,24 @@
 package com.potato.domain.comment.repository;
 
+import com.potato.domain.comment.BoardComment;
+import com.potato.domain.comment.BoardCommentRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.potato.domain.comment.QBoardComment.boardComment;
 
 @RequiredArgsConstructor
 public class BoardCommentRepositoryCustomImpl implements BoardCommentRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
+    @Override
+    public List<BoardComment> findBoardCommentById(Long boardId) {
+        return queryFactory.selectFrom(boardComment)
+            .where(
+                boardComment.boardId.eq(boardId)
+            ).fetch();
+    }
 }
